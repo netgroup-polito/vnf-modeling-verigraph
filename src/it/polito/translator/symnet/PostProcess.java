92542d2b6aa300a40fc1777847093ae89c8ad139
sandra211483 <34163203+sandra211483@users.noreply.github.com>
2018-09-19 23:47:15 +0200

    add PostProcess in SymNet Translation (generate :==: method)

---
+++ src/it/polito/translator/symnet/PostProcess.java
@@ -0,0 +1,69 @@
+package it.polito.translator.symnet;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Post-Process of SymNet Translator. It takes the Rule_NF.scala generated
+ * automatically by the parser. It reads each line of the file to replace
+ * symbolic values. <br>
+ * For example,<br>
+ * postParsef -> :==:
+ * 
+ * @author s211483
+ * @version 1.0 01/07/2018
+ *
+ */
+public class PostProcess {
+	public static void main(String[] args) {
+
+		FileInputStream fstream = null;
+		DataInputStream in = null;
+		BufferedWriter out = null;
+
+		try {
+			if (args.length != 2) {
+				System.err.println("Usage: Post_Parser <NF.scala_path>");
+				System.exit(-1);
+			}
+			fstream = new FileInputStream(args[0]);
+			in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			StringBuilder fileContent = new StringBuilder();
+			while ((strLine = br.readLine()) != null) {
+				if (strLine.contains("postParsef")) {
+					String strLineNew = strLine.replaceAll("postParsef", ":==:");
+					fileContent.append(strLineNew);
+					fileContent.append(System.getProperty("line.separator"));
+				} else if (strLine.contains(" Array[ConfigParameter]")) {
+					String ns = strLine.replace(" Array[ConfigParameter]", "List[ConfigParameter]");
+					fileContent.append(ns);
+					fileContent.append(System.getProperty("line.separator"));
+				} else {
+					fileContent.append(strLine);
+					fileContent.append(System.getProperty("line.separator"));
+				}
+			}
+			FileWriter fstreamWrite = new FileWriter(args[1]);
+			out = new BufferedWriter(fstreamWrite);
+			out.write(fileContent.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fstream.close();
+				out.flush();
+				out.close();
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
