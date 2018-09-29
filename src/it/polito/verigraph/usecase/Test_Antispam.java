345a15fd248668ec3d2a55ddbdcf93e536dd4b13
sandra211483 <34163203+sandra211483@users.noreply.github.com>
2018-09-29 16:40:04 +0200

    new test in Verigraph

---
+++ src/it/polito/verigraph/usecase/Test_Antispam.java
@@ -0,0 +1,136 @@
+package it.polito.verigraph.usecase;
+
+/**
+ * <p/>  Custom test  <p/>
+ *  | A | <------> | spam |<------> | B |
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.microsoft.z3.Context;
+import com.microsoft.z3.DatatypeExpr;
+import com.microsoft.z3.FuncDecl;
+import com.microsoft.z3.Model;
+import com.microsoft.z3.Status;
+import com.microsoft.z3.Z3Exception;
+
+import it.polito.verigraph.mcnet.components.Checker;
+import it.polito.verigraph.mcnet.components.IsolationResult;
+import it.polito.verigraph.mcnet.components.NetContext;
+import it.polito.verigraph.mcnet.components.Network;
+import it.polito.verigraph.mcnet.components.NetworkObject;
+import it.polito.verigraph.mcnet.components.Tuple;
+import it.polito.verigraph.mcnet.netobjs.PolitoEndHost;
+import it.polito.verigraph.mcnet.netobjs.PolitoMailServer;
+import it.polito.verigraph.mcnet.netobjs.PacketModel;
+import it.polito.verigraph.mcnet.netobjs.PolitoAntispam;
+
+public class Test_Antispam {
+
+    public Checker check;
+    public Context ctx;
+    public PolitoEndHost a;
+    public PolitoEndHost b;
+    public PolitoAntispam spam;
+
+    public  Test_Antispam(){
+        ctx = new Context();
+
+        NetContext nctx = new NetContext (ctx,new String[]{"a", "b","spam"},
+                new String[]{"ip_a", "ip_b", "ip_spam"});
+        Network net = new Network (ctx,new Object[]{nctx});
+
+        a = new PolitoEndHost(ctx, new Object[]{nctx.nm.get("a"), net, nctx});
+        b = new PolitoEndHost(ctx, new Object[]{nctx.nm.get("b"), net, nctx});
+        spam = new PolitoAntispam(ctx, new Object[]{nctx.nm.get("spam"), net, nctx});
+
+
+        ArrayList<Tuple<NetworkObject,ArrayList<DatatypeExpr>>> adm = new ArrayList<Tuple<NetworkObject,ArrayList<DatatypeExpr>>>();
+        ArrayList<DatatypeExpr> al1 = new ArrayList<DatatypeExpr>();
+        ArrayList<DatatypeExpr> al2 = new ArrayList<DatatypeExpr>();
+        ArrayList<DatatypeExpr> al3 = new ArrayList<DatatypeExpr>();
+        al1.add(nctx.am.get("ip_a"));
+        al2.add(nctx.am.get("ip_b"));
+        al3.add(nctx.am.get("ip_spam"));
+        adm.add(new Tuple<>(a, al1));
+        adm.add(new Tuple<>(b, al2));
+        adm.add(new Tuple<>(spam, al3));
+        net.setAddressMappings(adm);
+
+        ArrayList<Tuple<DatatypeExpr,NetworkObject>> rt1 = new ArrayList<Tuple<DatatypeExpr,NetworkObject>>();
+        rt1.add(new Tuple<DatatypeExpr,NetworkObject>(nctx.am.get("ip_spam"), spam));
+        rt1.add(new Tuple<DatatypeExpr,NetworkObject>(nctx.am.get("ip_b"), spam));
+
+        net.routingTable(a, rt1);
+
+        ArrayList<Tuple<DatatypeExpr,NetworkObject>> rt2 = new ArrayList<Tuple<DatatypeExpr,NetworkObject>>();
+        rt2.add(new Tuple<DatatypeExpr,NetworkObject>(nctx.am.get("ip_a"), spam));
+        rt2.add(new Tuple<DatatypeExpr,NetworkObject>(nctx.am.get("ip_spam"), spam));
+
+
+        net.routingTable(b, rt2);
+
+        ArrayList<Tuple<DatatypeExpr,NetworkObject>> rt3 = new ArrayList<Tuple<DatatypeExpr,NetworkObject>>();
+        rt3.add(new Tuple<DatatypeExpr,NetworkObject>(nctx.am.get("ip_a"),a));
+        rt3.add(new Tuple<DatatypeExpr,NetworkObject>(nctx.am.get("ip_b"),b));
+
+        net.routingTable(spam, rt3);
+
+
+        net.attach(a, b, spam);
+
+        //Configuring middleboxes
+        int[] s1= {3,4,8};
+        spam.addBlackList(s1);
+        
+        
+        PacketModel packet = new PacketModel();
+        packet.setEmailFrom(7);
+        a.installAsPOP3MailClient(nctx.am.get("ip_b"),packet);
+        b.installAsPOP3MailServer(new PacketModel());
+
+        check = new Checker(ctx,nctx,net);
+    }
+
+    public void resetZ3() throws Z3Exception{
+        HashMap<String, String> cfg = new HashMap<String, String>();
+        cfg.put("model", "true");
+        ctx = new Context(cfg);
+    }
+
+    public void printVector (Object[] array){
+        int i=0;
+        System.out.println( "*** Printing vector ***");
+        for (Object a : array){
+            i+=1;
+            System.out.println( "#"+i);
+            System.out.println(a);
+            System.out.println(  "*** "+ i+ " elements printed! ***");
+        }
+    }
+
+    public void printModel (Model model) throws Z3Exception{
+        for (FuncDecl d : model.getFuncDecls()){
+            System.out.println(d.getName() +" = "+ d.toString());
+            System.out.println("");
+        }
+    }
+
+
+    public static void main(String[] args) throws Z3Exception
+    {
+        Test_Antispam model = new Test_Antispam();
+        model.resetZ3();
+
+        IsolationResult ret =model.check.checkIsolationProperty(model.a,model.b);
+        model.printVector(ret.assertions);
+        if (ret.result == Status.UNSATISFIABLE){
+            System.out.println("UNSAT"); // Nodes a and b are isolated
+        }else{
+            System.out.println("**************************************************");
+            System.out.println("SAT ");
+            System.out.println(ret.model);
+        }
+    }
+}
